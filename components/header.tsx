394a33fd3f0c8114af33097b2fd0917281094fd5
Héctor Tovar <h.tovarm07@gmail.com>
2025-02-15 15:44:45 -0600

    fixed the header

--- components/header.tsx
+++ components/header.tsx
@@ -3,24 +3,26 @@ import Link from "next/link";
 import { socialNetworks } from "@/data";
 
 const Header = () => {
-    return ( 
-            <header>
-                <div className = "container justify-between max-w-6xl mx-auto md:flex">
-                    <Link href = "/">
-                    </Link>
-                    <div className="flex items-center justify-center gap-7">
-                        {socialNetworks.map(({logo,src,id}) => (
-                            <Link key={id}
+    return (
+        <header className="absolute z-40 w-full top-5 md:top-10 px-20">
+            <div className="container justify-between max-w-6xl mx-auto md:flex">
+                <Link href="/">
+                    {/* Add your logo or text here */}
+                </Link>
+                <div className="flex items-center justify-center gap-7">
+                    {socialNetworks.map(({ logo, src, id }) => (
+                        <Link
+                            key={id}
                             href={src}
-                            target ="_blank"
+                            target="_blank"
                             className="transition-all duration-300 hover:text-blue-500"
-                            >
-                                {logo}
-                            </Link>
-                        ))}
-                    </div>
+                        >
+                            {logo}
+                        </Link>
+                    ))}
                 </div>
-            </header>
+            </div>
+        </header>
     );
 }
  
